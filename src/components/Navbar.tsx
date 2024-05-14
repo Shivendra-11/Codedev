bdcd834a7d8af145dedee16b886c683eda3ed6db
shivendra <shivendrakeshari581@gmail.com>
2024-05-14 23:53:37 +0530

    initialize

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React, { act, useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
+
+
+
+const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
+  return (
+  
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)} >
+      <Menu setActive={setActive} >
+        <Link href={"/"} >
+        <MenuItem setActive={setActive}  active={active} item="Home"  >
+
+        </MenuItem>
+        
+        </Link>
+        {/* second navbar (courses) */}
+        <MenuItem setActive={setActive} active={active} item="our courses">
+            <div className="flex flex-col space-y-4 text-sm" >     
+                   <HoveredLink href="/web-dev"> </HoveredLink>
+                   <HoveredLink href="/interface-design">Interface Design</HoveredLink>
+            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
+            <HoveredLink href="/branding">Branding</HoveredLink>
+
+</div>
+        </MenuItem>
+
+        {/* contact  */}
+        <Link href={"/contact"} >
+         
+         
+        <MenuItem setActive={setActive} active={active} item="contact us"  >
+
+       
+        </MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar;
